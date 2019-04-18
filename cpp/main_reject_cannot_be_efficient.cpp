204740f7cfdcf192b81d4f368cee83bdbedf42f4
Yohsuke Murase <yohsuke.murase@gmail.com>
2019-04-18 16:41:57 +0900

    MPI-parallelize reject_cannot_be_efficient

--- cpp/main_reject_cannot_be_efficient.cpp
+++ cpp/main_reject_cannot_be_efficient.cpp
@@ -18,35 +18,58 @@ using namespace std;
 #endif
 
 int main(int argc, char** argv) {
-  if( argc != 2 ) {
+  MPI_Init(&argc, &argv);
+
+  if( argc != 3 ) {
     cerr << "Error : invalid argument" << endl;
-    cerr << "  Usage : " << argv[0] << " <strategy_file>" << endl;
+    cerr << "  Usage : " << argv[0] << " <strategy_file> <outfile>" << endl;
     return 1;
   }
+
+  int my_rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  int num_procs = 0;
+  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+  ifstream fin(argv[1]);
+  std::string out_format = argv[2];
+  char outfile[256];
+  sprintf(outfile, (out_format+".%d").c_str(), my_rank);
+  std::ofstream fout(outfile);
 
   int n_passed = 0;
   int n_rejected = 0;
 
-  ifstream fin(argv[1]);
-  vector<Strategy> ins;
   int count = 0;
   for( string s; fin >> s; count++ ) {
     if(count % 1000 == 0) {
-      std::cerr << "step: " << count << std::endl;
-      std::cerr << "passed / rejected : " << n_passed << " / " << n_rejected << std::endl;
+      std::cerr << my_rank << " : step : " << count << std::endl;
+      std::cerr << my_rank << " : passed / rejected : " << n_passed << " / " << n_rejected << std::endl;
     }
-    Strategy str(s.c_str());
-    if(str.ActionAt("cccccc") != C ) { cerr << str << std::endl;}
-    assert(str.ActionAt("cccccc") == C);
-    if( str.CannotBeEfficient() ) {
-      n_rejected++;
-    }
-    else {
-      n_passed++;
-      std::cout << s << std::endl;
+    if( count % num_procs == my_rank ) {
+      Strategy str(s.c_str());
+      if(str.ActionAt("cccccc") != C ) { cerr << str << std::endl;}
+      assert(str.ActionAt("cccccc") == C);
+      if( str.CannotBeEfficient() ) {
+        n_rejected++;
+      }
+      else {
+        n_passed++;
+        fout << s << std::endl;
+      }
     }
   }
-  std::cerr << "passed / rejected : " << n_passed << " / " << n_rejected << std::endl;
+  std::cerr << my_rank << " : passed / rejected : " << n_passed << " / " << n_rejected << std::endl;
+
+  int sum_n_passed = 0;
+  int sum_n_rejected = 0;
+  MPI_Reduce(&n_passed, &sum_n_passed, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  MPI_Reduce(&n_rejected, &sum_n_rejected, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  if( my_rank == 0 ) {
+    std::cerr << "[sum] : passed / rejected : " << sum_n_passed << " / " << sum_n_rejected << std::endl;
+  }
+
+  MPI_Finalize();
   return 0;
 }
 
