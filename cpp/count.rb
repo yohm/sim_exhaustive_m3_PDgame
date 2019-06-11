624734fa2d88782a9129e73e73daabd22e74b512
Yohsuke Murase <yohsuke.murase@gmail.com>
2019-06-11 11:01:53 +0900

    support wild card

--- cpp/count.rb
+++ cpp/count.rb
@@ -1,5 +1,4 @@
 require 'pp'
-require 'pry'
 
 def f(i)
   str = i.to_s.reverse
@@ -8,16 +7,18 @@ def f(i)
 end
 
 all_total = 0
-ARGV.each do |file|
-  total = 0
-  File.open(file).each do |l|
-    str,n_rej = l.chomp.split
-    n_w = str.count('*') + str.count('_')
-    num = 2**n_w - n_rej.to_i
-    total += num
+ARGV.each do |a|
+  Dir.glob(a).each do |file|
+    total = 0
+    File.open(file).each do |l|
+      str,n_rej = l.chomp.split
+      n_w = str.count('*') + str.count('_')
+      num = 2**n_w - n_rej.to_i
+      total += num
+    end
+    puts "#{file}:\t #{f(total)}"
+    all_total += total
   end
-  puts "#{file}:\t #{f(total)}"
-  all_total += total
 end
 
 puts f(all_total)
