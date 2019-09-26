4917ed34ce40ec83e854f9af52b73d5fa33b8422
Yohsuke Murase <yohsuke.murase@gmail.com>
2019-09-26 22:38:18 +0900

    print ITG from state 62

--- cpp/run_print_ITG.sh
+++ cpp/run_print_ITG.sh
@@ -5,5 +5,6 @@ abspath=`greadlink -f "$1"`
 IN_DIR=$(dirname "$abspath")
 
 cd "$IN_DIR"
-"$SCRIPT_DIR/cmake-build-release/main_print_ITG" "$abspath" cccccd > itg_cccccd.txt
+# "$SCRIPT_DIR/cmake-build-release/main_print_ITG" "$abspath" cccccd > itg_cccccd.txt
+"$SCRIPT_DIR/cmake-build-release/main_print_ITG" "$abspath" dddddc > itg_dddddc.txt
 
