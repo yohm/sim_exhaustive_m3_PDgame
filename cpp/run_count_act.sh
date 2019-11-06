4d642730cd3b7d8566a9a18441153004f9266963
Yohsuke Murase <yohsuke.murase@gmail.com>
2019-11-06 16:41:28 +0900

    refactoring

--- cpp/run_count_act.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-abspath=`greadlink -f "$1"`
-IN_DIR=$(dirname "$abspath")
-
-cd "$IN_DIR"
-ruby "$SCRIPT_DIR/count_act.rb" "$abspath" > count_act.txt
-
--- cpp/run_print_ITG.sh
+++ cpp/run_print_ITG.sh
@@ -6,5 +6,4 @@ IN_DIR=$(dirname "$abspath")
 
 cd "$IN_DIR"
 "$SCRIPT_DIR/cmake-build-release/main_print_ITG" "$abspath" cccccd > itg_cccccd.txt
-"$SCRIPT_DIR/cmake-build-release/main_print_ITG" "$abspath" dddddc > itg_dddddc.txt
 
